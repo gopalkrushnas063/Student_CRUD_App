304aa3740fe3899f2c0356a99669bf7aa82298aa
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2023-04-06 23:17:21 +0530

    application.properties added

--- src/test/java/com/example/student_crud_app/StudentCrudAppApplicationTests.java
+++ src/test/java/com/example/student_crud_app/StudentCrudAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StudentCrudAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
