ccc04ca48e11207815c5f7719204ad45e092ce1e
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2023-04-06 23:14:25 +0530

    Initial commit

---
+++ src/test/java/com/example/student_crud_app/StudentCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_crud_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
